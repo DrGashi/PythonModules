4b548aed70c20aac649905f5520ba51377245681
DrnGshi <drengashids@gmail.com>
2026-03-04 18:46:37 +0100

    .

--- Module10/challenge.py
+++ Module10/challenge.py
@@ -23,7 +23,7 @@ class DigitalSchool:
     def state(self):
         return self.__state
     @state.setter
-    def name(self, state):
+    def state(self, state):
         self.__state = state
 
     @property
@@ -43,8 +43,45 @@ class DigitalSchool:
     def organize_hackathon(self):
         pass
 
-class DS_Prishtina(DigitalSchool):
+class Ds_Prishtina(DigitalSchool):
     def __init__(self, name, city, state, courses, student_number):
         super().__init__(name, city, state, courses)
         self.__student_number = student_number
-    def SCF
+    @property
+    def student_number(self):
+        return self.__student_number
+    @student_number.setter
+    def student_number(self, student_number):
+        self.__student_number = student_number
+    def SCF(self):
+        print(f"The Spring Code Fest will be held in may in {self.city}.")
+    def organize_hackathon(self):
+        print("The Hackathon will be held in Kino Armata, Prishtine")
+
+class Ds_Prizren(DigitalSchool):
+    def __init__(self, name, city, state, courses, address):
+        super().__init__(name, city, state, courses)
+        self.__address = address
+    @property
+    def address(self):
+        return self.__address
+    @address.setter
+    def address(self, address):
+        self.__address = address
+
+    def organize_hackathon(self):
+        print("The Hackathon will be held in Prishtina so we have to travel by bus")
+    def print_houses(self):
+        print(f"{self.name} has the following houses: Hipsters, Engineers, Shadows and Speedsters")
+
+ds_prsh = Ds_Prishtina("Digital School Prishtina", "Prishtina", "Kosovo", "Python", 10000)
+
+ds_prsh.SCF()
+ds_prsh.organize_hackathon()
+print(f"The number of students in DS_Prishtina is {ds_prsh.student_number}")
+print()
+
+ds_prz = Ds_Prizren("Digital School Prizren", "Prizren", "Kosovo", "Web Development", "De Rada, M25, Prizren")
+ds_prz.organize_hackathon()
+ds_prz.print_houses()
+print(f"The address of Digital School Prizren is {ds_prz.address}")
