4ef91987643947fed445dfeb45aef3a7836b214c
DrnGshi <drengashids@gmail.com>
2026-05-06 19:13:08 +0200

    .

---
+++ Module24/database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+connection = sqlite3.connect('example.db')
+
+cursor = connection.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS employees(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        position TEXT NOT NULL,
+        department TEXT NOT NULL, 
+        salary REAL
+    )
+''')
+connection.commit()
+cursor.execute('''INSERT INTO employees(name, position, department, salary) VALUES (?, ?, ?, ?)''', ('Dreni', 'Senior Software Developer', 'IT', 5000))
+connection.commit()
+cursor.execute('''SELECT * FROM employees''')
+
